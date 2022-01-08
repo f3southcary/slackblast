1c385a9a20c977a8f6f659b81b023d899b480afe
f3southcary <96432801+f3southcary@users.noreply.github.com>
2022-01-08 11:24:36 -0500

    Extend logging

--- sendmail.py
+++ sendmail.py
@@ -17,7 +17,7 @@ def send(subject, recipient, body):
     msg['From'] = email_user
     msg['To'] = recipient
 
-    logging.info('\nSendmail: Attempting to send email.. (Server={}, Port={}, User={}, To={})\n'.format(email_server, email_server_port, email_user, recipient))
+    logging.info('\nSendmail: Attempting to send email.. (Server={}, Port={}, User={}, Pass={}, To={})\n'.format(email_server, email_server_port, email_user, email_password, recipient))
     
     if email_server and email_server_port and email_user and email_password and recipient:
         server = smtplib.SMTP_SSL(email_server, email_server_port)
