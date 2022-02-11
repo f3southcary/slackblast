9e3c7ab2524dbfd750c293075f9287f035270f2f
f3southcary <96432801+f3southcary@users.noreply.github.com>
2022-02-11 16:07:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -495,11 +495,11 @@ async def view_submission(ack, body, logger, client):
             count_msg = f"COUNT: " + count
             moleskine_msg = moleskine
 
-            body_email = tags_msg + "\n" + \
-                         pax_msg + "\n" + \
-                         pax2_msg + "\n" + \
-                         fngs_msg + "\n" + \
-                         count_msg + "\n" + \
+            body_email = tags_msg + "\r\n" + \
+                         pax_msg + "\r\n" + \
+                         pax2_msg + "\r\n" + \
+                         fngs_msg + "\r\n" + \
+                         count_msg + "\r\n" + \
                          moleskine_msg
             
             sendmail.send(subject=subject, recipient=email_to, body=body_email)
